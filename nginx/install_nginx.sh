5b718e9f6da89bf2eb240beb78182eeda71cd152
hrishikesh <hrishikesh@Hrishikeshs-MBP.home.ukb>
2019-12-01 12:04:28 +0000

    Remove default config

--- nginx/install_nginx.sh
+++ nginx/install_nginx.sh
@@ -24,7 +24,8 @@ mkdir -p /var/www/html/stats
 
 # Configuration setup.
 
-rm -f /etc/nginx/conf.d/default.conf 
+rm -f /etc/nginx/conf.d/default.conf
+rm -f /etc/nginx/sites-enabled/default 
 cat <<+++ >/etc/nginx/conf.d/db.conf
 # Default server configuration
 #
